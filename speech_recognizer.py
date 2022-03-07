54365af3ac20ffeab791592acd648f42c8bd0fe0
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2022-03-07 21:38:24 +0100

    Tokenizer trained successfully

--- speech_recognizer.py
+++ speech_recognizer.py
@@ -1,16 +1,27 @@
 from fastapi import FastAPI
-from prepare_dataset import prepare_mini_librispeech
+from prepare_dataset import prepare_mini_librispeech, skip
+from tokenizer import Tokenizer
 
 app = FastAPI()
 @app.get("/")
 async def root():
     try:
         prepare_mini_librispeech("dataset/",
-                                 "dataset/json-train.txt",
-                                 "dataset/json-valid.txt",
-                                 "dataset/json-test.txt")
+                                 "dataset/specification/train.json",
+                                 "dataset/specification/valid.json",
+                                 "dataset/specification/test.json")
     except Exception as e:
         print("There has been an error in preparing dataset " + str(e))
 
-    return {"message": "API Created succfvessfully !!!"}
+    if not skip("dataset/tokenizer/1000_unigram.model"):
+        try:
+            Tk = Tokenizer()
+            Tk.run()
+        except Exception as e:
+            print("There had been an error training the tokenizer "+ str(e))
+
+    else:
+        print("Tokenizer already trained")
+
+    return {"message": "API Created successfully !!!"}
 
---
+++ tokenizer.py
@@ -0,0 +1,49 @@
+import sys
+from speechbrain.tokenizers.SentencePiece import SentencePiece
+from hyperpyyaml import load_hyperpyyaml
+from prepare_dataset import prepare_mini_librispeech
+
+class Tokenizer:
+
+    yaml_file = open("tokenizer.yaml")
+    parsed_yaml_file = load_hyperpyyaml(yaml_file)
+
+    output_folder: str = parsed_yaml_file["output_folder"]
+    token_output: int = parsed_yaml_file["token_output"]
+    train_annotation: str = parsed_yaml_file["train_annotation"]
+    valid_annotation: str = parsed_yaml_file["valid_annotation"]
+    annotation_read: str = parsed_yaml_file["annotation_read"]
+    token_type: str = parsed_yaml_file["token_type"]
+    character_coverage: float = parsed_yaml_file["character_coverage"]
+
+    def __init__(self):
+        pass
+
+    def run(self):
+
+
+        #output_folder = parsed_yaml_file["output_folder"]
+        #print(output_folder)
+        print("Training the tokenizer...")
+
+        spm = SentencePiece(
+            model_dir= self.output_folder,
+            vocab_size= self.token_output ,
+            annotation_train= self.train_annotation,
+            annotation_read= self.annotation_read,
+            model_type= self.token_type,
+            character_coverage= self.character_coverage,
+            annotation_list_to_check= [self.train_annotation, self.valid_annotation],
+            annotation_format= "json",
+        )
+
+        print(" \n Tokenizer trained successfully!")
+
+
+# Testing the tekonizer
+import sentencepiece as spm
+sp = spm.SentencePieceProcessor()
+sp.load("dataset/tokenizer/1000_unigram.model")
+
+print(sp.encode_as_pieces('The city of Tunis'))
+
